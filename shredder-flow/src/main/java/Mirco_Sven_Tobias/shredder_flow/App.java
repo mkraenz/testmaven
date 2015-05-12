49eb3637cb160fd6954d713234f817df012397c8
Mirco Kraenz <prosingularity@yahoo.de>
2015-05-12 22:19:12 +0200

    INIT Maven project, set java compiler compliance to 1.7

---
+++ shredder-flow/src/main/java/Mirco_Sven_Tobias/shredder_flow/App.java
@@ -0,0 +1,13 @@
+package Mirco_Sven_Tobias.shredder_flow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
